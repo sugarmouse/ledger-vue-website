b1a8731f929316ea01c5f175fb74dad2433a43bb
sugarmouse <tanghao19950911@gmail.com>
2022-05-08 06:53:24 +0800

    deploy

---
+++ precache-manifest.d7cf1f7c79441f5121cf51a507bdaf58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e1ac7d87b504b2279c",
+    "url": "/ledger-vue-website/css/app.c3003162.css"
+  },
+  {
+    "revision": "f3c7e1eb821b35159a61962092459a78",
+    "url": "/ledger-vue-website/index.html"
+  },
+  {
+    "revision": "d1e1ac7d87b504b2279c",
+    "url": "/ledger-vue-website/js/app.99e86e82.js"
+  },
+  {
+    "revision": "addd334beaa37c6bcc82",
+    "url": "/ledger-vue-website/js/chunk-vendors.7278e8dd.js"
+  },
+  {
+    "revision": "1c0ce05dfda85ef14b5d9f511c322e15",
+    "url": "/ledger-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ledger-vue-website/robots.txt"
+  }
+]);
